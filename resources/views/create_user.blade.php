243f2747a74dbd42847aa02b442ccc54f9a0cde6
FatimaAlmashhor <fatima.amin4official@gmail.com>
2022-04-19 01:51:02 +0300

    refactor : register

--- resources/views/create_user.blade.php
+++ resources/views/create_user.blade.php
@@ -5,72 +5,64 @@
     <title>Login 08</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="/assets/client/css/main.css">
 
 </head>
 
 <body style="background-color:#FFFFFF ">
     <section class="ftco-section">
-        <div class="container">
+        <div class="container  ">
             <div class="row justify-content-center">
-                <div class="col-md-6 text-center mb-5">
-                    <h2 class="heading-section">Login #05</h2>
-                </div>
-            </div>
-            <div class="row justify-content-center">
-                <div class="col-md-7 col-lg-5">
-                    <div class="wrap">
-                        <div class="img"
-                            style="background-image:url(images/xbg-1.jpg.pagespeed.ic.EtoN2PmO7Y.webp)"></div>
-                        <div class="login-wrap p-4 p-md-5">
-                            <div class="d-flex">
-                                <div class="w-100">
-                                    <h3 class="mb-4">Sign In</h3>
-                                </div>
-                                <div class="w-100">
-                                    <p class="social-media d-flex justify-content-end">
-                                        <a href="#"
-                                            class="social-icon d-flex align-items-center justify-content-center"><span
-                                                class="fa fa-facebook"></span></a>
-                                        <a href="#"
-                                            class="social-icon d-flex align-items-center justify-content-center"><span
-                                                class="fa fa-twitter"></span></a>
-                                    </p>
+                <div class="col-md-6 col-lg-5">
+                    <div class="login-wrap p-4 p-md-5 border my-5">
+
+                        <h3 class="text-center mb-4">{{ __('login.create_account') }}</h3>
+                        <p style="text-align: center"> {{ __('login.start_joriny') }}!</p>
+                        <form action="#" class="login-form">
+                            <div class="form-group mb-2">
+                                <label for="username" class="form-label">{{ __('login.name') }}</label>
+                                <input type="text" class="form-control rounded-left" placeholder="Enter Your Name"
+                                    required name="name">
+                                <span class="text-danger">{{ $errors->first('name') }}</span>
+                            </div>
+                            <div class="form-group mb-2">
+                                <label for="text" class="form-label">{{ __('login.email') }}</label>
+                                <input type="password" class="form-control rounded-left" placeholder="Enter Your Email"
+                                    required name="email">
+                                <span class="text-danger">{{ $errors->first('email') }}</span>
+                            </div>
+                            <div class="mb-3 form-password-toggle">
+                                <label class="form-label" for="password">{{ __('login.password') }}</label>
+                                <div class="input-group input-group-merge">
+                                    <input style="height: 38px;" type="password" class="form-control" name="user_pass"
+                                        placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                                        aria-describedby="password" name="password" />
+                                    <span class="text-danger">{{ $errors->first('password') }}</span>
                                 </div>
                             </div>
-                            <form action="#" class="signin-form">
-                                <div class="form-group mt-3">
-                                    <input type="text" class="form-control" required>
-                                    <label class="form-control-placeholder" for="username">Username</label>
+                            <div class="mb-3 form-password-toggle">
+                                <label class="form-label" for="password">{{ __('login.cpassword') }}</label>
+                                <div class="input-group input-group-merge">
+                                    <input style="height: 38px;" type="password" class="form-control"
+                                        name="confirm_pass"
+                                        placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                                        aria-describedby="password" name="cpassword" />
+                                    <span class="text-danger">{{ $errors->first('cpassword') }}</span>
+                                    {{-- <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span> --}}
                                 </div>
-                                <div class="form-group">
-                                    <input id="password-field" type="password" class="form-control" required>
-                                    <label class="form-control-placeholder" for="password">Password</label>
-                                    <span toggle="#password-field"
-                                        class="fa fa-fw fa-eye field-icon toggle-password"></span>
-                                </div>
-                                <div class="form-group">
-                                    <button type="submit" class="form-control btn btn-primary rounded submit px-3">Sign
-                                        In</button>
-                                </div>
-                                <div class="form-group d-md-flex">
-                                    <div class="w-50 text-left">
-                                        <label class="checkbox-wrap checkbox-primary mb-0">Remember Me
-                                            <input type="checkbox" checked>
-                                            <span class="checkmark"></span>
-                                        </label>
-                                    </div>
-                                    <div class="w-50 text-md-right">
-                                        <a href="#">Forgot Password</a>
-                                    </div>
-                                </div>
-                            </form>
-                            <p class="text-center">Not a member? <a data-toggle="tab" href="#signup">Sign Up</a></p>
-                        </div>
+                            </div>
+                            <button class="wak_btn d-grid w-100">{{ __('login.register') }}
+                            </button>
+                            <button class="wak_btn green_border w-100 mt-3">
+                                {{ __('login.register') }} مع Google
+                            </button>
+                            <p class="text-center mt-3">
+                                <span>{{ __('login.have_account') }}?</span>
+                                <a href="auth-login-basic.html">
+                                    <span style="color: #0d41fd">Sign in instead</span>
+                                </a>
+                            </p>
+                        </form>
                     </div>
                 </div>
             </div>
