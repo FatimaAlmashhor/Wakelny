28277621dd3cc2555b47babf2a1102e7c33e0c90
Dhoha Alkhorasani <raihanahit2016@gmail.com>
2022-04-20 02:00:08 +0300

    Design: user identity section

--- resources/views/client/userProfile/userProfile.blade.php
+++ resources/views/client/userProfile/userProfile.blade.php
@@ -1,13 +1,30 @@
 @extends('client.master_layout')
 @section('content')
     <!-- Page Content-->
-    <div class="container-fluid border-bottom p-5 d-flex">
-        <div class="profile-identity d-flex justify-content-between">
+    <div class="container-fluid border-bottom p-5">
+        <div class="profile-identity d-flex justify-content-start">
             <div class="profile-card--avatar shadow-sm rounded-circle">
                 <img src="../../assets/client/images/user-profile-2.png" class="rounded-circle profile-avatar">
+                <div class="is_active-dot rounded-circle"></div>
             </div>
 
-            
+            <div class="user-info color-black d-flex flex-column justify-content-center mt-5 py-0">
+                <div class="username">
+                    <!-- <h5>اسم المستخدم</h5> -->
+                    <h5>ضحى الخراساني</h5>
+                </div>
+
+                <div class="user-brief text-muted">
+                    <p class="d-inline-block ms-3">
+                        <!-- <i class="fas fa-briefcase"></i> <span class="me-1">التخصص</span> -->
+                        <i class="fas fa-briefcase"></i> <span class="me-1">متخصصة في برمجة مواقع الويب Full Stack Developer</span>
+                    </p>
+                    <p class="d-inline-block">
+                        <!-- <i class="fa-solid fa-location-dot"></i> <span class="me-1">البلد</span> -->
+                        <i class="fa-solid fa-location-dot"></i> <span class="me-1">اليمن</span>
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
