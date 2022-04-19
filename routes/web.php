a8e7c82fdd6fbecc61510f4f7641c8f86a8960a5
RuqaiahSaif <ruqaiah.saif@gmail.com>
2022-04-19 23:42:06 +0300

    Roqia/crud_category

--- routes/web.php
+++ routes/web.php
@@ -42,7 +42,7 @@ Route::group([
     Route::view('/', 'client.static.home');
     Route::view('/aboutUs', 'client.static.about_us');
     Route::view('/contactUs', 'client.static.contactUs');
-    
+
 Route::get('/users',[AuthController::class,'listAll'])->name('users');
 Route::get('/create_user',[AuthController::class,'create'])->name('create_user');
 Route::post('/save_user',[AuthController::class,'register'])->name('save_user');
@@ -66,9 +66,19 @@ Route::post('/save_skill',[SkillController::class,'store'])->name('save_skill');
 Route::get('/edit_skill/{skill_id}',[SkillController::class,'edit'])->name('edit_skill');
 Route::get('/toggle_skill/{skill_id}',[SkillController::class,'toggle'])->name('toggle_skill');
 Route::post('/update_skill/{skill_id}',[SkillController::class,'update'])->name('update_skill');
+
+//////////////////////CRUD category ////////////////
+Route::get('/list_categories',[CategoriesController::class,'list_category'])->name('list_categories');
+Route::get('/add_category',[CategoriesController::class,'add_category'])->name('add_category');
+Route::get('/edit_category/{cat_id}',[CategoriesController::class,'edit'])->name('edit_category');
+Route::get('/toggle_category/{cat_id}',[CategoriesController::class,'toggle'])->name('toggle_category');
+Route::post('/save_category',[CategoriesController::class,'store'])->name('save_category');
+Route::post('/update_category/{cat_id}',[CategoriesController::class,'update'])->name('update_category');
+
+
 });
 
-  
+
 //start  roles managment
 Route::get('/generate_roles', [SettingsController::class, 'generateRoles'])->name('generate_roles');
 //end roles managment
