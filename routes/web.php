bd8a3692122db20fd7195501fabf50a0ec46dba1
FatimaAlmashhor <fatima.amin4official@gmail.com>
2022-04-20 11:56:51 +0300

    Fix : web conflict

--- routes/web.php
+++ routes/web.php
@@ -29,60 +29,55 @@ use App\Http\Controllers\admin\SkillController;
 Route::get('/generate_roles', [SettingsController::class, 'generateRoles'])->name('generate_roles');
 //end roles managment
 
-Route::get('/users',[AuthController::class,'listAll'])->name('users');
-Route::get('/create_user',[AuthController::class,'create'])->name('create_user');
-Route::post('/save_user',[AuthController::class,'register'])->name('save_user');
-Route::get('/reset_password',[AuthController::class,'resetpass'])->name('reset_password');
-Route::get('/login',[AuthController::class,'showLogin'])->name('login');
-Route::post('/do_login',[AuthController::class,'login'])->name('do_login');
-Route::get('/logout',[AuthController::class,'logout'])->name('logout');
+Route::get('/users', [AuthController::class, 'listAll'])->name('users');
+Route::get('/create_user', [AuthController::class, 'create'])->name('create_user');
+Route::post('/save_user', [AuthController::class, 'register'])->name('save_user');
+Route::get('/reset_password', [AuthController::class, 'resetpass'])->name('reset_password');
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/do_login', [AuthController::class, 'login'])->name('do_login');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
 
 
-// ------------------------------------------------------------------------
-// Client section
-// ------------------------------------------------------------------------
+
 Route::group([
     'prefix' => LaravelLocalization::setLocale(),
     'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']
 ], function () {
-    Route::get('/',[ControllPannelController::class,'index'])->name('home');
+
+
+    // ------------------------------------------------------------------------
+    // Client section
+    // ------------------------------------------------------------------------
+    Route::get('/', [ControllPannelController::class, 'index'])->name('home');
     Route::view('/aboutUs', 'client.static.about_us');
     Route::view('/contactUs', 'client.static.contactUs');
 
 
- 
 
+    // ------------------------------------------------------------------------
+    // Admin section
+    // ------------------------------------------------------------------------
+    Route::group(['middleware' => ['auth', 'role:admin']], function () {
+        Route::get('/admin', [ControllPannelController::class, 'admin'])->name('admin');
+        //////////////////////CRUD skills ////////////////
+        Route::get('/skills', [SkillController::class, 'listAll'])->name("skills");
+        Route::get('/create_skill', [SkillController::class, 'create'])->name('create_skill');
+        Route::post('/save_skill', [SkillController::class, 'store'])->name('save_skill');
+        Route::get('/edit_skill/{skill_id}', [SkillController::class, 'edit'])->name('edit_skill');
+        Route::get('/toggle_skill/{skill_id}', [SkillController::class, 'toggle'])->name('toggle_skill');
+        Route::post('/update_skill/{skill_id}', [SkillController::class, 'update'])->name('update_skill');
 
-
-
-
-
-// ------------------------------------------------------------------------
-// Admin section
-// ------------------------------------------------------------------------
-Route::group(['middleware'=>['auth','role:admin']],function(){
-Route::get('/admin',[ControllPannelController::class,'admin'])->name('admin');
-//////////////////////CRUD skills ////////////////
-Route::get('/skills',[SkillController::class,'listAll'])->name("skills");
-Route::get('/create_skill',[SkillController::class,'create'])->name('create_skill');
-Route::post('/save_skill',[SkillController::class,'store'])->name('save_skill');
-Route::get('/edit_skill/{skill_id}',[SkillController::class,'edit'])->name('edit_skill');
-Route::get('/toggle_skill/{skill_id}',[SkillController::class,'toggle'])->name('toggle_skill');
-Route::post('/update_skill/{skill_id}',[SkillController::class,'update'])->name('update_skill');
-
-//////////////////////CRUD category ////////////////
-Route::get('/list_categories',[CategoriesController::class,'list_category'])->name('list_categories');
-Route::get('/add_category',[CategoriesController::class,'add_category'])->name('add_category');
-Route::get('/edit_category/{cat_id}',[CategoriesController::class,'edit'])->name('edit_category');
-Route::get('/toggle_category/{cat_id}',[CategoriesController::class,'toggle'])->name('toggle_category');
-Route::post('/save_category',[CategoriesController::class,'store'])->name('save_category');
-Route::post('/update_category/{cat_id}',[CategoriesController::class,'update'])->name('update_category');
+        //////////////////////CRUD category ////////////////
+        Route::get('/list_categories', [CategoriesController::class, 'list_category'])->name('list_categories');
+        Route::get('/add_category', [CategoriesController::class, 'add_category'])->name('add_category');
+        Route::get('/edit_category/{cat_id}', [CategoriesController::class, 'edit'])->name('edit_category');
+        Route::get('/toggle_category/{cat_id}', [CategoriesController::class, 'toggle'])->name('toggle_category');
+        Route::post('/save_category', [CategoriesController::class, 'store'])->name('save_category');
+        Route::post('/update_category/{cat_id}', [CategoriesController::class, 'update'])->name('update_category');
+    });
 });
-});
-});
+
 //start  roles managment
 Route::get('/generate_roles', [SettingsController::class, 'generateRoles'])->name('generate_roles');
 //end roles managment
-
-
