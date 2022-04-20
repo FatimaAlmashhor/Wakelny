d6138e0e7ce4a9d57b8fa9abfabe039f794d3dd3
Dhoha Alkhorasani <raihanahit2016@gmail.com>
2022-04-20 03:53:58 +0300

    Add: user profile menue

--- resources/views/client/userProfile/userProfile.blade.php
+++ resources/views/client/userProfile/userProfile.blade.php
@@ -1,15 +1,15 @@
 @extends('client.master_layout')
 @section('content')
-    <!-- Page Content-->
     <div class="container-fluid border-bottom p-5">
-        <div class="profile-identity row"> <!-- d-flex justify-content-start   col-lg-3 col-md-4 col-sm-12  -->
+        <!-- User Identety Brief-->
+        <div class="profile-identity row"> 
             <div class="profile-card--avatar shadow-sm rounded-circle">
                 <img src="../../assets/client/images/user-profile-2.png" class="rounded-circle profile-avatar">
                 <div class="is_active-dot rounded-circle"></div>
             </div>
 
             <div class="user-info color-black mt-5 py-0 col-md-8">
-                <div class="username">
+                <div class="username color-black">
                     <!-- <h5>اسم المستخدم</h5> -->
                     <h5>ضحى الخراساني</h5>
                 </div>
@@ -26,5 +26,14 @@
                 </div>
             </div>
         </div>
+        <!-- /User Identety Brief-->
+
+        <!-- Profile Nav -->
+        <nav class="user-profile-nav nav fw-bold">
+            <a class="nav-link active color-black" aria-current="page" href="#">حول</a>
+            <a class="nav-link color-black" href="#">الأعمال</a>
+            <a class="nav-link color-black" href="#">التقييمات</a>
+        </nav>
+        <!-- /Profile Nav -->
     </div>
 @endsection
