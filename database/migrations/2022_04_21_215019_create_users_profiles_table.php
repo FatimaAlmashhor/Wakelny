95efeab9dda16e30198039c9a6d6b3071f66160d
Dhoha Alkhorasani <raihanahit2016@gmail.com>
2022-04-22 01:29:37 +0300

    Update: code format of forign key of users_profiles migration

--- database/migrations/2022_04_21_215019_create_users_profiles_table.php
+++ database/migrations/2022_04_21_215019_create_users_profiles_table.php
@@ -14,15 +14,17 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users_profiles', function (Blueprint $table) {
-            $table->bigIncrements('profile_id');
+            // $table->bigIncrements('profile_id');
+            $table->foreignId('user_id')->constrained()
+                                        ->onUpdate('cascade')
+                                        ->onDelete('cascade');
             $table->string('name');
             $table->string('avatar')->nullable();
             $table->string('gender')->nullable();
             $table->string('mobile');
+            $table->string('specialization');
             $table->longText('bio');
             $table->timestamps();
-
-            $table->foreign('profile_id')->references('id')->on('users');
         });
     }
 
