0b2ae0e7f380a825176f5c5908e0817e2f4bb474
Dhoha Alkhorasani <raihanahit2016@gmail.com>
2022-04-24 01:08:50 +0300

    Delete: genereteRoles function from siteController.php

--- app/Http/Controllers/admin/SettingsController.php
+++ app/Http/Controllers/admin/SettingsController.php
@@ -8,31 +8,5 @@ use Illuminate\Http\Request;
 
 class SettingsController extends Controller
 {
-    function generateRoles(){
-
-        Role::create([
-            'name' => 'super_admin',
-            'display_name' => 'ادارة النظام ', // optional
-            //'description' => 'User is the owner of a given project', // optional
-        ]);
-
-        Role::create([
-            'name' => 'admin',
-            'display_name' => 'ادارة المحتوى', // optional
-            //'description' => 'User is the owner of a given project', // optional
-        ]);
-
-        Role::create([
-            'name' => 'seeker',
-            'display_name' => 'طالب الخدمة', // optional
-            //'description' => 'User is the owner of a given project', // optional
-        ]);
-
-        Role::create([
-            'name' => 'provider',
-            'display_name' => 'مقدم الخدمة', // optional
-            //'description' => 'User is the owner of a given project', // optional
-        ]);
-
-    }
+    //
 }
