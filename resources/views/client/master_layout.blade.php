0462aaa5057becbf45d4862a2161a65226bd243f
Dhoha Alkhorasani <raihanahit2016@gmail.com>
2022-04-24 14:16:52 +0300

    Add JavaScript Bundle with Popper script to master

--- resources/views/client/master_layout.blade.php
+++ resources/views/client/master_layout.blade.php
@@ -47,6 +47,9 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
+    <!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 
     <script src="/assets/client/js/helper/jquery-3.6.0.min.js"></script>
