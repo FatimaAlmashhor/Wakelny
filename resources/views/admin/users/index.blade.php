a91fd7df993c1db8ff4cc72aa555c24f956258c1
Mohammed Aldoais <mohammedaldoais1996@gmail.com>
2022-05-01 05:07:52 +0300

    add page Admin_Blockusers

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,7 +1,7 @@
 
 @extends('admin.master_layout')
 @section('content')
-    {{-- titile --}}
+    {{-- titile ----}}
     <header class="mb-3">
         <a href="#" class="burger-btn d-block d-xl-none">
             <i class="bi bi-justify fs-3"></i>
