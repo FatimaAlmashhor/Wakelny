bfa518ea51a19d14fe74576facf3589e97a61bed
afnanalkadasi <afnanalkadasi22@gmail.com>
2022-05-04 17:36:43 +0300

    make report of user and content and show it in dash and active or no active

---
+++ app/Http/Controllers/admin/ReportController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Report;
+use App\Models\Posts;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class ReportController extends Controller
+{
+    ////////////////////show report in dashboard///////////
+    public function showAll(){
+        $reports =  report::select(
+            'reports.id',
+            'reports.user_id',
+            'reports.post_id',
+            'reports.provider_id',
+            'reports.type_report',
+            'reports.massege',
+            'profiles.name'
+        )->join('profiles', 'profiles.user_id', '=', 'reports.user_id')->where('is_active', 1)->get();
+    
+
+        return view('admin.report.index')->with(['reports'=>$reports ]);
+
+    }
+
+    public function store(Request $request)
+    {
+       
+            $request->validate([
+                'type_report' => ['required'],
+                'massege' => ['required', 'min:100'],
+                
+            ], [
+                'type_report.required' => 'يجب ان تقوم بأدخال  نوع البلاغ',
+                'massege.required' => 'اضف وصف للبلاغ',
+                'massege.min' => 'حقل الوصف يجب ان يحتوي على 255 حرف على الاقل',
+            ]);
+
+          
+
+            $report = new report();
+            $report->user_id = Auth::id();
+            $report->post_id = $request->post_id;
+            $report->provider_id = $request->provider_id;
+            $report->type_report = $request->type_report;
+            $report->massege = $request->massege;
+          
+    
+           
+
+            if ($report->save()) {
+
+                return redirect()->route('freelancers')
+                    ->with(['message' => 'تم اضافة بلاغ  بنجاح', 'type' => 'alert-success']);
+            } else
+                return back()->with(['message' => 'فشلت عمليه الاضافة الرجاء اعاده المحاوله   ', 'type' => 'alert-danger']);
+       
+    }
+    public function toggle($report_id){
+        $report=report::find($report_id);
+        $report->is_active*=-1;
+        if($report->save())
+            if($report->is_active==-1)
+                    return back()->with(['message' => 'تم تعطيل البلاغ بنجاح', 'type' => 'alert-success']);
+                else
+                    return back()->with(['message' => 'تم تفعيل البلاغ بنجاح', 'type' => 'alert-success']);
+        return back()->with(['message' => 'فشلت عمليه الحذف الرجاء اعاده المحاوله   ', 'type' => 'alert-danger']);
+    
+    }
+}
---
+++ app/Models/work.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class work extends Model
+{
+    use HasFactory;
+    
+    public function getImageAttribute($value){
+        return url("images/")."/".$value;
+
+    }
+}
