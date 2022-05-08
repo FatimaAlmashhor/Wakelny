581427792867d64be0c470c3bcf0887191d0817d
Dhoha Alkhorasani <raihanahit2016@gmail.com>
2022-05-08 20:21:00 +0300

    Modify the name of Evaluation to EvaluationController and remanigm the Rating.php file to EvaluationController.php

--- app/Http/Controllers/client/EvaluationController.php
+++ app/Http/Controllers/client/EvaluationController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\client;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class Evaluation extends Controller
+class EvaluationController extends Controller
 {
     //
 }
