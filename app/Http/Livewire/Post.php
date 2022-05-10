f94a37b3a094b409d848bc8da952abffb3247e30
FatimaAlmashhor <fatima.amin4official@gmail.com>
2022-05-10 13:08:03 +0300

    Fix: livewire problem

--- app/Http/Livewire/Post.php
+++ app/Http/Livewire/Post.php
@@ -14,10 +14,31 @@ class Post extends Component
     public function render()
     {
         if (empty($this->searchTerm)) {
-            $this->posts = Posts::all();
+            $this->posts = Posts::select(
+                'posts.id as post_id',
+                'posts.user_id',
+                'posts.title',
+                'posts.offers',
+                'posts.description',
+                'profiles.name',
+                'profiles.user_id as provider_id'
+            )->join('profiles', 'profiles.user_id', '=', 'posts.user_id')->where('is_active', 1)->where('status', 'open')->orderBy('id', 'DESC')->get();
             $this->categories = category::get();
         } else {
-            $this->posts = Posts::where('title', 'like', '%' . $this->searchTerm . '%')->get();
+            $this->posts = Posts::select(
+                'posts.id as post_id',
+                'posts.user_id',
+                'posts.title',
+                'posts.offers',
+                'posts.description',
+                'profiles.name',
+                'profiles.user_id as provider_id'
+            )->join('profiles', 'profiles.user_id', '=', 'posts.user_id')
+                ->where('is_active', 1)
+                ->where('status', 'open')
+                ->where('title', 'like', '%' . $this->searchTerm . '%')
+                ->orderBy('id', 'DESC')
+                ->get();
         }
         return view('livewire.post');
     }
