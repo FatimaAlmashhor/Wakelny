b3a7e8f0f274d44078cf6624b74c29bc2ec3770d
afnanalkadasi <afnanalkadasi22@gmail.com>
2022-05-12 00:09:12 +0300

    redesgin contact us page and desgin wellat of admin

--- routes/web.php
+++ routes/web.php
@@ -89,7 +89,8 @@ Route::group([
     Route::view('/', 'client.static.home')->name('home');
     Route::view('/aboutUs', 'client.static.about_us')->name('aboutus');
     Route::view('/contactUs', 'client.static.contactUs')->name('contactus');
-
+    Route::view('/wallet', 'admin.wallet.wallet')->name('wallet');
+    
     // this is the page of the freelancers
     Route::get('/freelancers', [UserController::class, 'index'])->name('freelancers');
     // this is the subsection of howen the freelncers
