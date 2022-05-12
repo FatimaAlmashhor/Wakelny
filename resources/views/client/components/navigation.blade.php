c7e1cc028be64516d3386c0ffe5cd100d13fb54b
FatimaAlmashhor <fatima.amin4official@gmail.com>
2022-05-12 12:04:55 +0300

    Fix: logout

--- resources/views/client/components/navigation.blade.php
+++ resources/views/client/components/navigation.blade.php
@@ -1,201 +1,91 @@
-<nav class="navbar navbar-expand-lg px-5 main-nav">
-    <!-- <div class="container-fluid"> -->
-    <a class="navbar-brand fw-bold fs-3 brand" href="{{ route('home') }}">كلفني</a>
+<!-- header -->
+<header class="w-full bg-primary-blue h-fit flex justify-center items-center shadow-lg border-md p-2 px-3 text-white ">
 
-    <button
-        class="navbar-toggler text-light collapsed
-                        d-flex d-lg-none flex-column justify-content-around
-                        "
-        data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-        aria-expanded="false" aria-label="Toggle navigation">
-
-        <span class="toggler-icon top-bar"></span>
-        <span class="toggler-icon middle-bar"></span>
-        <span class="toggler-icon bottom-bar"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-                <a class="nav-link active fw-bolder nav-links" aria-current="page"
-                    href="{{ route('home') }}">الرئيسية</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link nav-links" href="{{ route('freelancers') }}">المستقلون</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link fs-6 nav-links" href="{{ route('projectlancer') }}">المشاريع المتاحه </a>
-            </li>
-        </ul>
-
-        @if (Auth::check())
-            @role('seeker')
-                <p>{{ auth()->user()->role }}</p>
-
-                <ul class="navbar-nav  ">
-                    <li class="nav-item ">
-                        <a class="nav-link fs-6 nav-links" href="{{ route('myProject') }}">مشاريعي </a>
+    <!-- navigation -->
+    <nav x-data="{ isOpen: false }" @keydown.escape="isOpen = false" id='nav'
+        class="flex  items-center justify-between md:flex-rowflex-col md:flex-row  w-full h-fit p-3 px-8"
+        :class="{ 'shadow-lg flex-col border-md': isOpen, '': !isOpen }">
+        <div class=" flex justify-between md:w-fit w-full">
+            <div class="logo-font">
+                <p>متاح</p>
+            </div>
+            <!-- toggle menu  -->
+            <!-- <div class="inline md:hidden cursor-pointer" @click="isNavOpend = !isNavOpend">close</div> -->
+            <button @click="isOpen = !isOpen" type="button"
+                class="block md:hidden px-2 text-gray-500 hover:text-white focus:outline-none focus:text-white"
+                :class="{ 'transition transform-180': isOpen }">
+                <svg class="h-6 w-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <path x-show="isOpen" style='stroke: white; fill: white;' fill-rule="evenodd" clip-rule="evenodd"
+                        d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z" />
+                    <path x-show="!isOpen" style='stroke: white; fill: white;' fill-rule="evenodd"
+                        d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z" />
+                </svg>
+            </button>
+        </div>
+        <!-- the nav content -->
+        <div :class="{ 'block shadow-3xl': isOpen, 'hidden': !isOpen }" @click.away="isOpen = false"
+            x-show.transition="true"
+            class="hidden md:flex flex-col md:flex-row mt-10 md:mt-0 w-full h-full md:items-center justify-between">
+            <div class="mx-6 flex-1 mb-10 md:mb-0 ">
+                <ul class="flex flex-col md:flex-row gap-x-4 ">
+                    <li class="nav_item font-sm cursor-pointer active_link">
+                        <a href="{{ route('home') }}">
+                            الرئسية
+                        </a>
+                    </li>
+                    <li class="nav_item font-sm cursor-pointer">
+                        <a href="{{ route('projectlancer') }}">
+                            طالبي الخدمات
+                        </a>
+                    </li>
+                    <li class="nav_item font-sm cursor-pointer">
+                        <a href="{{ route('freelancers') }}">
+                            مقدمي الخدمات
+                        </a>
                     </li>
                 </ul>
-            @endrole
-        @endif
+            </div>
+            @if (Auth::guest())
+                <div class="flex gap-x-3 ">
+                    <a href="{{ route('login') }}"
+                        class="mo-btn btn-light-pink-bg p-2 px-6 rounded-full bg-primary-light-pink text-black">
+                        تسجيل الدخول
+                    </a>
+                    <a href="{{ route('create_user') }}" class="mo-btn btn-light-pink-rounderd">
+                        حساب جديد
+                    </a>
 
-        @if (Auth::check())
-            @role('provider')
-                <p>{{ auth()->user()->role }}</p>
-
-                <ul class="navbar-nav  ">
-                    <li class="nav-item ">
-                        <a class="nav-link fs-6 nav-links" href="{{ route('myWorks') }}">اعمالي </a>
-                    </li>
-                    <li class="nav-item ">
-                        <a class="nav-link fs-6 nav-links" href="{{ route('workonProject') }}">اعمل على </a>
-                    </li>
-                </ul>
-            @endrole
-        @endif
-
-
-
-        @if (Auth::check())
-
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex jusify-content-center align-item-center">
-
-                <li class="nav-item ms-2 user-items">
-                    <a class="nav-link color-offwhite fs-5" href="#"><i
-                            class="fa-solid fa-magnifying-glass font-sm"></i></a>
-                </li>
-                <li class="nav-item ms-2 user-items">
-                    <a class="nav-link color-offwhite fs-5" href="#"><i
-                            class="fa-solid fa-comment-dots font-sm"></i></a>
-                </li>
-                <li class="nav-item ms-2 user-items">
-                    <div class="nav-link color-offwhite fs-5 " aria-expanded="false" type="button"
-                        id="dropdownMenuButton1" data-bs-toggle="dropdown" style="position: relative">
-                        <i class="fa-solid fa-bell font-sm"></i>
-                        @if (auth()->user()->unreadNotifications->count() > 0)
-                            <span
-                                style=" position: absolute; width:12px; height:12px ; border-radius : 50% ;background:red ; bottom: 10px ; left: 20px;">
-                                {{-- {{ auth()->user()->unreadNotifications->count() }} --}}
-                            </span>
-                        @endif
+                </div>
+            @endif
+            @if (Auth::check() && !Auth::user()->hasRole('admin'))
+                <div @click.away="open = false" class="relative" x-data="{ open: false }">
+                    <button @click="open = !open"
+                        class="flex flex-row items-center w-full px-3 py-1 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg md:w-auto md:mt-0 md:ml-2 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
+                        <span class="text-lg text-primary"><i class="fas fa-bell text-white"></i></span>
+                    </button>
+                    <div x-show="open" x-transition:enter="transition ease-out duration-100"
+                        x-transition:enter-start="transform opacity-0 scale-95"
+                        x-transition:enter-end="transform opacity-100 scale-100"
+                        x-transition:leave="transition ease-in duration-75"
+                        x-transition:leave-start="transform opacity-100 scale-100"
+                        x-transition:leave-end="transform opacity-0 scale-95"
+                        class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48">
+                        <div class="px-2 py-2 bg-white rounded-md shadow">
+                            <a class="block px-4 py-2 mt-2  bg-transparent rounded-lg  text-sm font-semibold md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                                href="#">Notifikasi 1</a>
+                            <a class="block px-4 py-2 mt-2  bg-transparent rounded-lg  text-sm font-semibold md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                                href="#">Notifikasi #2</a>
+                            <a class="block px-4 py-2 mt-2  bg-transparent rounded-lg  text-sm font-semibold md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                                href="#">Notifikasi #3</a>
+                        </div>
                     </div>
+                </div>
+                <a class="flex items-center px-3 py-1 mt-2 text-lg font-semibold text-primary rounded-lg md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                    href="#">
+                    <i class="fas fa-envelope text-white"></i>
+                </a>
+            @endif
+        </div>
+    </nav>
 
-                    <ul class="dropdown-menu dropdown-menu-right mt-1 mx-5" aria-labelledby="dropdownMenuButton1"
-                        style="overflow: auto; width:340px ; hieght 70vh">
-
-                        @foreach (auth()->user()->unreadNotifications as $notification)
-                            <li class=""
-                                style="color: gray ; overflow-wrap: break-word; height: fit-content ;   ">
-                                <a class="dropdown-item color-black my-2 p-3" href="{{ $notification->data['url'] }}"
-                                    style=' color: gray ; border-right: 4px solid {{ $notification->read_at == null ? 'red' : 'gray' }}  ; padding-right: 2px ; width:inherit; height: fit-content; '>
-                                    @if ($notification->data['type'] == 'comment')
-                                        {{-- <a href="{{ route('markAsReadOne', $notification->id) }}"> --}}
-                                        <span> قام بأضافه
-                                            {{ $notification->data['name'] }}
-                                            عرض جديد على مشروعك</span>
-                                        {{-- </a> --}}
-                                    @else
-                                        {{-- <a href="{{ route('markAsReadOne', $notification->id) }}"> --}}
-                                        <span>
-                                            {{ $notification->data['message'] }}
-                                        </span>
-                                        {{-- </a> --}}
-                                    @endif
-
-                                </a>
-                            </li>
-                        @endforeach
-                    </ul>
-
-
-
-                </li>
-                <li class="dropdown rtl">
-
-                    <div class=" mt-2 rounded-circle position-relative  dropdown-toggle" aria-expanded="false"
-                        type="button" id="dropdownMenuButton1" style="width: 32px ; height: 32px;"
-                        data-bs-toggle="dropdown">
-                        {{-- @if (!Auth::user()->hasRole('admin'))
-                            @if ($item->avatar !== 'http://localhost:8000/images/')
-                                <img src="{{ $item->avatar }}" class="profile-avatar position-absoulte"
-                                    style="width: 100%; height:100%; object-fit: cover">
-                            @else
-                                <img src="/assets/client/images/user-profile-2.png"
-                                    class="profile-avatar position-absoulte dropdown-toggle"
-                                    style="width: 100%; height:100%; object-fit: cover">
-                            @endif
-                        @else
-                            <img src="/assets/client/images/user-profile-2.png"
-                                class="profile-avatar position-absoulte dropdown-toggle"
-                                style="width: 100%; height:100%; object-fit: cover">
-                        @endif --}}
-
-                        <img src="/assets/client/images/user-profile-2.png"
-                            class="profile-avatar position-absoulte dropdown-toggle"
-                            style="width: 100%; height:100%; object-fit: cover">
-                    </div>
-                    {{-- <img class="img-avatar rounded-circle dropdown-toggle" type="button" id="dropdownMenuButton1"
-                        data-bs-toggle="dropdown" aria-expanded="false"
-                        src={{ $item->avatar ?? '/assets/client/images/user-profile-2.png' }}> --}}
-
-                    <ul class="dropdown-menu dropdown-menu-right mt-2" aria-labelledby="dropdownMenuButton1">
-                        @if (!Auth::user()->hasRole('admin'))
-                            <li class="">
-                                <a class="dropdown-item color-black"
-                                    href="{{ route('userProfile', Auth::user()->id) }}">
-                                    <i class="fa-solid fa-user ms-1 font-xs"></i>
-                                    <span>{{ Auth::user()->name }}</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item color-black" href="#">
-                                    <i class="fa-solid fa-bookmark  ms-1 font-xs"></i>
-                                    <span>المفضلة</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item color-black" href="#">
-                                    <i class="fa-solid fa-dollar-sign ms-1 font-xs"></i>
-                                    <span>الرصيد</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item color-black" href="{{ route('profile') }}">
-                                    <i class="fa-solid fa-sliders ms-1 font-xs"></i>
-                                    <span>تعديل الحساب</span>
-                                </a>
-                            </li>
-                        @else
-                            <li>
-                                <a class="dropdown-item color-black" href="{{ route('admin') }}">
-                                    <i class="fa-solid fa-sliders ms-1 font-xs"></i>
-                                    <span>لوحه التحكم</span>
-                                </a>
-                            </li>
-                        @endif
-
-                        <hr>
-                        <li>
-                            <a class="dropdown-item color-black" href="{{ route('logout') }}">
-                                <i class="fa-solid fa-arrow-right-from-bracket ms-1 font-xs"></i>
-                                <span>تسجيل الخروج</span>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-        @elseif (Auth::guest())
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link fs-6 nav-links " href="{{ route('login') }}">تسجيل الدخول</a>
-                </li>
-
-                <li class="nav-item border">
-                    <a class="nav-link fs-6 nav-links " href="{{ route('create_user') }}">حساب جديد </a>
-                </li>
-            </ul>
-        @endif
-    </div>
-    <!-- </div> -->
-</nav>
+</header>
