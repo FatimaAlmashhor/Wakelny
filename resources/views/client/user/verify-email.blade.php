77bbb159c4eb9b86dad810ada55b29fb9b9e003f
Mohammed Aldoais <mohammedaldoais1996@gmail.com>
2022-05-14 00:10:33 +0300

    Add-Emaiveryfy-Animation

--- resources/views/client/user/verify-email.blade.php
+++ resources/views/client/user/verify-email.blade.php
@@ -20,16 +20,13 @@
                                     <p>رجاء قم بتأكيد البريد الاكتروني اولا لتتمكن من البدأ</p>
 
                                     <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-<lottie-player src="https://assets7.lottiefiles.com/packages/lf20_xumyfzqp.json"  background="transparent"  speed=".5"  style="width: 300px; height: 300px;"  loop  autoplay></lottie-player>
+                                    <lottie-player src="https://assets1.lottiefiles.com/packages/lf20_h1vps4vg.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop  autoplay></lottie-player>
                                     <form action="{{ route('verification.request') }}" method="post"
                                         class="my-4">
                                         @csrf
                                         <button class='mo-btn' type="submit">اضفط هنا لاعاده الارسال</button>
                                     </form>
                                 </div>
-                                </div>
-                                </div>
-                            </div>
                             </div>
                         </div>
 
