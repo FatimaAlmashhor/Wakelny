b26dc3299e9362619a90c7a4ad33710ff9e94a25
Mohammed Aldoais <mohammedaldoais1996@gmail.com>
2022-05-16 03:02:29 +0300

    Add-PAge invalides

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,7 +4,6 @@
         <div class="bg-white rounded-2xl border shadow-x1 p-10 " style='width:33rem'>
             <div class="flex flex-col items-center space-y-4">
                 <h1 class="font-bold text-4xl text-sacondary-red w-4/6 text-center">
-
                     !! Opps
                 </h1>
                 </h1>
@@ -15,8 +14,6 @@
                     </p>
 
                 </h1>
-
-
                 <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
                 <lottie-player src="https://assets9.lottiefiles.com/packages/lf20_avwze6mc.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop  autoplay></lottie-player>
 
