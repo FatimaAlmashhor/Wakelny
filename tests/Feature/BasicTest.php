a8811ecdb7275d5c46d3b99ae8f053c54179958f
FatimaAlmashhor <fatima.amin4official@gmail.com>
2022-05-18 10:36:58 +0300

    first laravel deployment to heroku

--- tests/Feature/BasicTest.php
+++ tests/Feature/BasicTest.php
@@ -3,18 +3,19 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class BasicTest extends TestCase
 {
     /**
-     * A basic test example.
+     * A basic feature test example.
      *
      * @return void
      */
-    public function test_the_application_returns_a_successful_response()
+    public function test_example()
     {
-        $response = $this->get('/ar');
+        $response = $this->get('/');
 
         $response->assertStatus(200);
     }
