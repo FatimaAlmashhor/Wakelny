0f6d354b77768de0e0841c83ffa2efa4ea9ddab3
FatimaAlmashhor <fatima.amin4official@gmail.com>
2022-05-19 20:31:13 +0300

    Fix : client wallet

--- app/Http/Controllers/admin/WalletController.php
+++ app/Http/Controllers/admin/WalletController.php
@@ -22,24 +22,6 @@ class WalletController extends Controller
             ->where('payment_status', 'received')
             ->first();
 
-        // $transactions = DB::table('transactions')->join('projects', 'projects.id', function ($q) {
-        // return $q->whereJsonContains('meta', ['project_id']);
-        // })->get();
-        // $transactions = Transaction::select(
-        //     "meta->'$.project_id'"
-        //     // 'projects.post_id',
-        //     // 'posts.title',
-        //     // 'transactions.amount',
-        //     // 'transactions.created_at'
-        // )
-        //     ->join('projects', 'projects.id', '=', 'JSON_CONTAINS(meta, \'{"project_id"}\')')
-        //     // ->where("meta->'project_id'", '!=', null)
-        //     //     ->where('payable_id', 1)
-        //     //     // ->whereJsonContains('meta', 'project_id')
-        //     //     // where('type', 'deposit')
-        //     //     // ->whereJsonContains('meta', ['project_id' => 'project_id'])
-        //     //     // ->join('posts', 'posts.id', '=', 'projects.post_id')
-        //     ->get();
         $transactions = Transfer::select(
             'profiles.name',
             'deposit.amount',
