c0f060b062fc241e9bb8e161fe82ea5b0832042f
RuqaiahSaif <ruqaiah.saif@gmail.com>
2022-05-21 00:35:20 +0300

    modifyPostDetailsNumbersInput2

--- app/Http/Controllers/client/postController.php
+++ app/Http/Controllers/client/postController.php
@@ -10,7 +10,7 @@ use App\Models\Posts;
 use App\Models\Project;
 use App\Models\PostSkills;
 use App\Models\Profile;
-use App\Models\Project;
+
 use App\Models\Skill;
 use App\Models\User;
 use App\Notifications\PostNotification;
