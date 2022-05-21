b1f6942f2001cd6fb767bce871ade549bf803bd0
Dhoha Alkhorasani <raihanahit2016@gmail.com>
2022-05-21 10:23:05 +0300

    Create: CommentTest file

--- tests/Unit/CommentTest.php
+++ tests/Unit/CommentTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Models\Comments;
 use PHPUnit\Framework\TestCase;
 
 class CommentTest extends TestCase
@@ -14,5 +15,18 @@ class CommentTest extends TestCase
     public function test_example()
     {
         $this->assertTrue(true);
+    }
+
+    public function testCommentCreation()
+    {
+        $comment = new Comments([
+            'duration' => 100,
+            'cost' => 100,
+            'cost_after_taxs' => 100,
+            'description' => 'Testing description using PHPUnit toole',
+            'is_active' => 1
+        ]);
+
+        $this->assertEquals(100, $comment->duration);
     }
 }
