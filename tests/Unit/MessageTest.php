3b3931f9ad96ef1b77ea25ea8bb1d493ade83052
Dhoha Alkhorasani <raihanahit2016@gmail.com>
2022-05-21 10:29:37 +0300

    Iplement: Message testing

--- tests/Unit/MessageTest.php
+++ tests/Unit/MessageTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Models\Messages;
 use PHPUnit\Framework\TestCase;
 
 class MessageTest extends TestCase
@@ -14,5 +15,17 @@ class MessageTest extends TestCase
     public function test_example()
     {
         $this->assertTrue(true);
+    }
+
+    public function testCommentCreation()
+    {
+        $message = new Messages([
+            'message' => 'Test Message',
+            'user_id' => 2,
+            'receiver' => 15,
+            'is_seen' => 1
+        ]);
+
+        $this->assertEquals('Test Message', $message->message);
     }
 }
