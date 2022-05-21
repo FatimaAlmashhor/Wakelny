454c3dd2a19abc1d748e80f31d20ce6d12116aa1
Dhoha Alkhorasani <raihanahit2016@gmail.com>
2022-05-21 10:44:45 +0300

    Implement WalletTest

--- tests/Unit/WalletTest.php
+++ tests/Unit/WalletTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Models\Wallet;
 use PHPUnit\Framework\TestCase;
 
 class WalletTest extends TestCase
@@ -14,5 +15,20 @@ class WalletTest extends TestCase
     public function test_example()
     {
         $this->assertTrue(true);
+    }
+
+    public function testWalletCreation()
+    {
+        $wallet = new Wallet([
+            'holder_type' => 'App\Model\User',
+            'holdet_id' => 2,
+            'name' => 'Default Wallet',
+            'slug' => 'default',
+            'uuid' => '510575d7-4744-4b6c-a056-a1d93afbc9b2',
+            'balance' => 10000,
+            'decimal_places' => 2
+        ]);
+
+        $this->assertEquals('App\Model\User', $wallet->holder_type);
     }
 }
