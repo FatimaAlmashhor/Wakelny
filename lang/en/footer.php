a803e4af6eebc0ecb4e48dd88da123fa9fc1ab36
afnanalkadasi <afnanalkadasi22@gmail.com>
2022-05-25 19:35:10 +0300

    update language

---
+++ lang/en/footer.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    "Links" => "Links",
+    "ContactUs"=>"  Contact Us  ",
+    "aboutus" => "About Us",
+    "Services" => "Services",
+    "text"=>" A creative platform that brings together business owners and freelance developers to facilitate work and guarantee rights with high quality and ease",
+    "help"=>"Help ",
+    "works"=>" Works",
+];
