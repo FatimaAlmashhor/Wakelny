19d57939f51b9349ab677c92f980ffb3e25ba0ee
FatimaAlmashhor <fatima.amin4official@gmail.com>
2022-06-04 18:34:36 +0300

    fixing

--- app/Http/Middleware/OnlineMiddleware.php
+++ app/Http/Middleware/OnlineMiddleware.php
@@ -20,13 +20,13 @@ class OnlineMiddleware
     public function handle(Request $request, Closure $next)
     {
         $currentTime = (string)(new \DateTime())->format('Y-m-d H:i:s');
-        $users_to_offline = User::where('last_activity', '<', '"' . $currentTime . '"')->first();
-        $users_to_online = User::where('last_activity', '>=', '"' . $currentTime . '"')->fisrt();
+        $users_to_offline = User::where('last_activity', '<', '"' . $currentTime . '"');
+        $users_to_online = User::where('last_activity', '>=', '"' . $currentTime . '"');
         if (isset($users_to_offline)) {
-            $users_to_offline->update(['is_online' => 0])->touch();
+            $users_to_offline->update(['is_online' => 0]);
         }
         if (isset($users_to_online)) {
-            $users_to_online->update(['is_online' => 1])->touch();
+            $users_to_online->update(['is_online' => 1]);
         }
         if (auth()->check()) {
             $cache_value = Cache::put('user-is-online', auth()->id(), \Carbon\Carbon::now()->addMinutes(1));
