c2c4a85265a112a7650f2ccc94e02de35c8031e3
FatimaAlmashhor <fatima.amin4official@gmail.com>
2022-06-04 18:43:56 +0300

    fixing

--- app/Http/Middleware/OnlineMiddleware.php
+++ app/Http/Middleware/OnlineMiddleware.php
@@ -19,28 +19,32 @@ class OnlineMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        $currentTime = (string)(new \DateTime())->format('Y-m-d H:i:s');
-        $users_to_offline = User::where('last_activity', '<', '"' . $currentTime . '"');
-        $users_to_online = User::where('last_activity', '>=', '"' . $currentTime . '"');
-        if (isset($users_to_offline)) {
-            $users_to_offline->update(['is_online' => 0]);
-        }
-        if (isset($users_to_online)) {
-            $users_to_online->update(['is_online' => 1]);
-        }
-        if (auth()->check()) {
-            $cache_value = Cache::put('user-is-online', auth()->id(), \Carbon\Carbon::now()->addMinutes(1));
-            $user = User::find(Cache::get('user-is-online'));
-            $user->last_activity = now()->addMinutes(1);
-            $user->is_online = true;
-            $user->save();
-        } elseif (!auth()->check() and filled(Cache::get('user-is-online'))) {
-            $user = User::find(Cache::get('user-is-online'));
-            if (isset($user)) {
-                $user->is_online = false;
-                $user->save();
+        try {
+            $currentTime = (string)(new \DateTime())->format('Y-m-d H:i:s');
+            $users_to_offline = User::where('last_activity', '<', '"' . $currentTime . '"');
+            $users_to_online = User::where('last_activity', '>=', '"' . $currentTime . '"');
+            if (isset($users_to_offline)) {
+                $users_to_offline->update(['is_online' => 0]);
             }
+            if (isset($users_to_online)) {
+                $users_to_online->update(['is_online' => 1]);
+            }
+            if (auth()->check()) {
+                $cache_value = Cache::put('user-is-online', auth()->id(), \Carbon\Carbon::now()->addMinutes(1));
+                $user = User::find(Cache::get('user-is-online'));
+                $user->last_activity = now()->addMinutes(1);
+                $user->is_online = true;
+                $user->save();
+            } elseif (!auth()->check() and filled(Cache::get('user-is-online'))) {
+                $user = User::find(Cache::get('user-is-online'));
+                if (isset($user)) {
+                    $user->is_online = false;
+                    $user->save();
+                }
+            }
+            return $next($request);
+        } catch (\Throwable $th) {
+            //throw $th;
         }
-        return $next($request);
     }
 }
