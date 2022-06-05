8d4c6a63cad4856a47f40fc9076d49217c7f60de
FatimaAlmashhor <fatima.amin4official@gmail.com>
2022-06-05 11:24:07 +0300

    fixing

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -18,13 +18,13 @@ class VerifyCsrfToken
     ];
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->guest()) {
-            if ($request->ajax()) {
-                return response('Unauthorized.', 401);
-            } else {
-                return redirect()->guest('login');
-            }
-        }
+        // if (Auth::guard($guard)->guest()) {
+        //     if ($request->ajax()) {
+        //         return response('Unauthorized.', 401);
+        //     } else {
+        //         return redirect()->guest('login');
+        //     }
+        // }
 
         return $next($request);
     }
